90c8d0e6dfd28fcc630084b4c694909a2f991c7d
Gede Indra Adi Brata <indrabrata599@gmail.com>
2023-05-02 20:03:14 +0700

    Updating main & handler

--- main.go
+++ main.go
@@ -4,11 +4,9 @@ import (
 	"gin/database/mysql"
 	"gin/database/supabase"
 	"gin/src/handler"
-	"gin/src/middleware"
 	"gin/src/repository"
 	"gin/src/usecase"
 	"log"
-	"os"
 
 	"github.com/gin-gonic/gin"
 )
@@ -16,7 +14,6 @@ import (
 var Router *gin.Engine
 
 func main() {
-	r := gin.Default()
 
 	db, err := mysql.SqlInit()
 
@@ -28,45 +25,9 @@ func main() {
 
 	db.RunMigration()
 
-	repo := repository.InitRepository(*db, supabase)
-	uc := usecase.InitUsecase(repo)
-	h := handler.InitHandler(uc)
+	repository := repository.InitRepository(*db, supabase)
+	usecase := usecase.InitUsecase(repository)
+	handler := handler.InitHandler(usecase)
 
-	r.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": os.Getenv("JWT_SIGN"),
-		})
-	})
-
-	v1 := r.Group("/api/v1")
-
-	user := r.Group(v1.BasePath() + "/user")
-	user.POST("/signup", h.UserRegister) //new user signup
-	user.POST("/login", h.UserLogin)     //user login
-	user.Use(middleware.IsUserLoggedIn).
-		PUT("/update", h.UserUpdate).               //user update profile without photo profile
-		POST("/upload/photo", h.UploadPhotoProfile) //user upload photo profile
-
-	eco := r.Group(v1.BasePath() + "/tourism")
-	eco.POST("/post", h.PostTourism) //post ecotourism
-	eco.Use(middleware.IsUserLoggedIn).
-		GET("/", h.GetAllTourism).                                       //get all eco tourism
-		GET("/:id", h.GetTourismByID).                                   //get eco tourism by id
-		GET("/filter/category/:category", h.GetTourismByCategory).       //get filtered eco tourism by category
-		GET("/filter/price/:startPrice/:endPrice", h.GetTourismByPrice). //get filtered eco tourism by price
-		GET("/filter/region/:region", h.GetTourismByRegion).             //get filtered
-		POST("/:id/comment", h.UploadComment)                            //comment on current eco tourism, id in here is id ecotourism
-
-	trash := r.Group(v1.BasePath() + "/trash")
-	trash.Use(middleware.IsUserLoggedIn).
-		POST("/exchange", h.ExchangeTrash).              //exchange trash into coin
-		GET("/exchange/history", h.ExchangeTrashHistory) //get exchange trash history
-
-	article := r.Group(v1.BasePath() + "/article")
-	article.Use(middleware.IsUserLoggedIn).
-		GET("/", h.GetAllArticles).      //get all articles
-		GET(":id", h.GetArticleByID).    //get article by id
-		POST("/create", h.CreateArticle) //create a article
-
-	r.Run()
+	handler.Run()
 }
--- src/handler/handler.go
+++ src/handler/handler.go
@@ -1,20 +1,72 @@
 package handler
 
 import (
+	"fmt"
+	"gin/src/middleware"
 	"gin/src/usecase"
+	"net/http"
+	"os"
 	"sync"
+
+	"github.com/gin-gonic/gin"
 )
 
 var once = sync.Once{}
 
 type Handler struct {
-	uc *usecase.Usecase
+	http *gin.Engine
+	uc   *usecase.Usecase
 }
 
 func InitHandler(uc *usecase.Usecase) *Handler {
 	r := Handler{}
 	once.Do(func() {
+		r.http = gin.Default()
 		r.uc = uc
+		r.RoutesAndMiddleware()
 	})
 	return &r
+}
+
+func (h *Handler) RoutesAndMiddleware() {
+	h.http.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Hello world",
+		})
+	})
+
+	v1 := h.http.Group("/api/v1")
+
+	user := h.http.Group(v1.BasePath() + "/user")
+	user.POST("/signup", h.UserRegister) //new user signup
+	user.POST("/login", h.UserLogin)     //user login
+	user.Use(middleware.IsUserLoggedIn).
+		PUT("/update", h.UserUpdate).               //user update profile without photo profile
+		POST("/upload/photo", h.UploadPhotoProfile) //user upload photo profile
+
+	eco := h.http.Group(v1.BasePath() + "/tourism")
+	eco.POST("/post", h.PostTourism) //post ecotourism
+	eco.Use(middleware.IsUserLoggedIn).
+		GET("/", h.GetAllTourism).                                       //get all eco tourism
+		GET("/:id", h.GetTourismByID).                                   //get eco tourism by id
+		GET("/filter/category/:category", h.GetTourismByCategory).       //get filtered eco tourism by category
+		GET("/filter/price/:startPrice/:endPrice", h.GetTourismByPrice). //get filtered eco tourism by price
+		GET("/filter/region/:region", h.GetTourismByRegion).             //get filtered
+		POST("/:id/comment", h.UploadComment)                            //comment on current eco tourism, id in here is id ecotourism
+
+	trash := h.http.Group(v1.BasePath() + "/trash")
+	trash.Use(middleware.IsUserLoggedIn).
+		POST("/exchange", h.ExchangeTrash).              //exchange trash into coin
+		GET("/exchange/history", h.ExchangeTrashHistory) //get exchange trash history
+
+	article := h.http.Group(v1.BasePath() + "/article")
+	article.Use(middleware.IsUserLoggedIn).
+		GET("/", h.GetAllArticles).      //get all articles
+		GET(":id", h.GetArticleByID).    //get article by id
+		POST("/create", h.CreateArticle) //create a article
+
+}
+
+func (h *Handler) Run() {
+	h.http.Run(fmt.Sprintf(":%s", os.Getenv("APP_PORT")))
 }
